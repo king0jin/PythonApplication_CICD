90ed34940504d20a3b86ec497df0506d3fdb1249
youngjini <et0709@naver.com>
2024-10-21 15:31:38 +0900

    views.py 수정

--- apiserver/views.py
+++ apiserver/views.py
@@ -4,5 +4,5 @@ from rest_framework import status
 
 @api_view(['GET'])
 def index(request):
-    data = {"result":"success", "data":[{"id":"jini", "name":"younjin"}]}
+    data = {"result":"success", "data":[{"id":"jini", "name":"kimyounjin"}]}
     return Response(data, status=status.HTTP_200_OK)
